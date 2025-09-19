92b6cc581edc8feae5dedd51ad9ee44e7bfc7652
ndirangu26206-netizen <ndirangu.26206@students.kyu.ac.ke>
2025-09-19 14:21:12 +0300

    Add files via upload

---
+++ personal inquiry .c
@@ -0,0 +1,20 @@
+/*
+Name:Evanson Karuri
+reg:CT100/G/26206/25
+description:my details program
+*/
+#include<stdio.h>
+int main(){
+  
+  int height;
+  scanf("%d",&height);
+  printf("my height is:%d\n",height);
+  
+  int phoneNo;
+  scanf("%d",&phoneNo);
+  printf("my phone number is:%d\n",phoneNo);
+ 
+  int weight;
+  scanf("%d",&weight);
+  printf("my weight is:%d",weight);
+}
---
+++ semester units.c
@@ -0,0 +1,18 @@
+/*
+Name:Evanson Karuri
+reg:CT100/G/26206/25
+description:my semester units program
+*/
+
+#include<stdio.h>
+int main(){
+  printf("my units are:\n1.analogue electronics\n");
+  printf("2.mathematics for science\n");
+  printf("3.communication skills\n");
+  printf("4.computer systems and organization\n");
+  printf("5.essentials of accounting\n");
+  printf("6.application of computer\n");
+  printf("7.fundamentals of ICT\n");
+  printf("8.introduction to programming and algorithms"); 
+}
+  
